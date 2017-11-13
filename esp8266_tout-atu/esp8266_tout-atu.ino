f9c3d31f9722f848c62357750d3e654513b483cc
dignified0012 <dignified@hotmail.co.jp>
2017-11-14 01:40:54 +0900

    Add files via upload

---
+++ esp8266_tout-atu/esp8266_tout-atu.ino
@@ -0,0 +1,26 @@
+#include <ESP8266WiFi.h>
+extern "C" {
+#include "user_interface.h"
+}
+void setup() {
+  Serial.begin(115200);
+  pinMode(12, OUTPUT);
+  Serial.println("PG start");
+}
+
+void loop() {
+
+  //unsigned int 型の入れ物を用意
+  uint ADC_Value = 0;
+
+  //AD変換実行
+  ADC_Value = system_adc_read();
+
+  //計測結果をシリアルに書き出す。0-1024の値
+  if(ADC_Value > 0){
+    digitalWrite(12, HIGH);
+    Serial.println("ANALOG == "+ String(ADC_Value));
+    delay(500);
+    digitalWrite(12, LOW);
+  }
+}
---
+++ esp8266_tout-d/esp8266_tout-d.ino
@@ -0,0 +1,22 @@
+#include <ESP8266WiFi.h>
+extern "C" {
+#include "user_interface.h"
+}
+void setup() {
+  Serial.begin(115200);
+  Serial.println("PG start");
+}
+
+void loop() {
+
+  //unsigned int 型の入れ物を用意
+  uint ADC_Value = 0;
+
+  //AD変換実行
+  ADC_Value = system_adc_read();
+
+  //計測結果をシリアルに書き出す。0-1024の値
+  if(ADC_Value > 0){Serial.println("ANALOG == "+ String(ADC_Value));}
+  delay(1);
+
+}
